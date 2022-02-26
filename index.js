4a87fde4673c4a54a4ac918584b586a0f16a1907
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-02-26 15:44:33 +0600

    socket.io added

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+//import express and call it
 const express = require('express')
 const app = express()
+
+//import http and create server
+const http = require('http');
+const server = http.Server(app);
+
+//import socket.io and create IO server with http server
+const io = require('socket.io')(server);
+
 const cors = require('cors');
 const port = process.env.PORT || 5000
 
-const eWss = require('express-ws')(app);
 
 /*
 const server = require('http').createServer(app);
@@ -21,26 +29,18 @@ wss.on("message", function incoming(message) {
 */
 app.use(express.json());
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + './index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
+    });
+});
+
+http.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-app.ws('/', (ws, req) => {
-    ws.on("connection", (wss) => {
-        console.log("A new client connected");
-        wss.send("Welcome new user");
-    });
-
-    ws.on("message", function incoming(message) {
-        console.log("received %s", message);
-
-        eWss.getWss().clients.forEach(function each(client) {
-            client.send(message);
-        })
-    })
-
-})
