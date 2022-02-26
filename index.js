ec3bd3e9b4300119457b0bac956a0af37f9baca5
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-02-26 16:12:12 +0600

    updated

--- index.js
+++ index.js
@@ -1,44 +1,18 @@
-//import express and call it
 const app = require('express')();
-
-//import http and create server
 const http = require('http').Server(app);
-
-//import socket.io and create IO server with http server
 const io = require('socket.io')(http);
-
-const cors = require('cors');
-const port = process.env.PORT || 5000;
-
-
-/*
-const server = require('http').createServer(app);
-const Websocket = require('ws');
-const wss = new Websocket.Server({ server: server });
-
-wss.on("connection", function connection(ws) {
-    console.log("A new client connected");
-    ws.send("Welcome new client");
-});
-
-wss.on("message", function incoming(message) {
-    console.log("received %s", message);
-})
-*/
-app.use(express.json());
-app.use(cors());
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    socket.on('message', (msg) => {
-        io.emit('message', msg);
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
     });
 });
 
 http.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
