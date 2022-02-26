297f2ab1cae3ee03c7497f8fb4d59f8f88a64c2f
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-02-26 07:31:34 +0600

    express ws added

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
 
+const eWss = require('express-ws')(app);
+
+app.ws('/', (ws, req) => {
+    ws.on("connection", (ws) => {
+        console.log("A new client connected");
+        ws.send("Welcome new user");
+    });
+
+    ws.on("message", function incoming(message) {
+        console.log("received %s", message);
+    })
+
+})
+
+/*
 const server = require('http').createServer(app);
 const Websocket = require('ws');
-
 const wss = new Websocket.Server({ server: server });
 
 wss.on("connection", function connection(ws) {
@@ -16,6 +30,7 @@ wss.on("connection", function connection(ws) {
 wss.on("message", function incoming(message) {
     console.log("received %s", message);
 })
+*/
 
 app.use(cors());
 app.get('/', (req, res) => {
