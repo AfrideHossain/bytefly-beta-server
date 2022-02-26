79e33222cf2aa73e27e3d6084b59028f07b07685
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-02-26 16:25:53 +0600

    Updated

--- index.js
+++ index.js
@@ -9,22 +9,6 @@ const io = require('socket.io')(http);
 
 const port = process.env.PORT || 5000;
 
-
-/*
-const server = require('http').createServer(app);
-const Websocket = require('ws');
-const wss = new Websocket.Server({ server: server });
-
-wss.on("connection", function connection(ws) {
-    console.log("A new client connected");
-    ws.send("Welcome new client");
-});
-
-wss.on("message", function incoming(message) {
-    console.log("received %s", message);
-})
-*/
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
