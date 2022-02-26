f615079153a87fab3d67c81f37479731a35d3d7f
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-02-26 16:14:43 +0600

    moded

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
+//import express and call it
 const app = require('express')();
+
+//import http and create server
 const http = require('http').Server(app);
+
+//import socket.io and create IO server with http server
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 5000;
+
+
+/*
+const server = require('http').createServer(app);
+const Websocket = require('ws');
+const wss = new Websocket.Server({ server: server });
+
+wss.on("connection", function connection(ws) {
+    console.log("A new client connected");
+    ws.send("Welcome new client");
+});
+
+wss.on("message", function incoming(message) {
+    console.log("received %s", message);
+})
+*/
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + './index.html');
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
     });
 });
 
 http.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
+
